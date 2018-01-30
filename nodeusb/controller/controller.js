f661873a5cc1712b20629f451633a1af6edc39b5
adolfo-lopez-rocher <adolfo.lopez.rocher@es.ibm.com>
2018-01-30 13:09:49 +0100

    hay que arreglar la escritura en el archivo

--- nodeusb/controller/controller.js
+++ nodeusb/controller/controller.js
@@ -1,3 +1,13 @@
 var usb = require ('usb');
+const fs = require('fs');
 
 console.log(usb.getDeviceList());
+
+var texto = usb.getDeviceList();
+
+fs.writeFile('prueba.txt', texto, (err) =>{
+    if (err) throw err;
+    console.log('guardado con éxito');
+})
+
+
