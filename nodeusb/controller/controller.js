2d389d4e89781231f1b9a68e7a4da5f44d75a150
adolfo-lopez-rocher <adolfo.lopez.rocher@es.ibm.com>
2018-01-31 12:06:27 +0100

    miniUpdate

--- nodeusb/controller/controller.js
+++ nodeusb/controller/controller.js
@@ -1,10 +1,8 @@
 var usb = require ('usb');
 const fs = require('fs');
+var util = require('util');
 
 var texto = usb.getDeviceList();
-
-console.log(texto);
-console.log(typeof(texto));
 
 str = JSON.stringify(texto);
 
@@ -13,4 +11,34 @@ fs.writeFile('prueba.txt', str, (err) =>{
    console.log('guardado con éxito');
 })
 
+function pasarArray(){
+    var array = str.split("Device");
+    console.log(array);
+    fs.writeFile('prueba2.txt', JSON.stringify(array), (err)=>{
+        if (err) throw err;
+        console.log('22222 con exito');
+    })
+}
+pasarArray();
+
+
+// var stream = require('stream');
+// var Stream = stream.Stream;
+
+// var ws = new Stream;
+// ws.writable = true;
+
+// ws.write = function(data) {
+//   console.log("input=" + data);
+// }
+
+// ws.end = function(data) {
+//     var str = "prueba";
+//     console.log("bye");
+//     fs.writeFile('asd.txt', str, (err) =>{
+//         if (err) throw err;
+//     })
+// }
+
+// process.stdin.pipe(ws);
 
